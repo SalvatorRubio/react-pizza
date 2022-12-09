ab500496932958bac1e63ad9e2df0b1a496f66cd
Matvey <matvejilchenko9388@gmail.com>
2022-12-09 16:10:23 +0300

    Инициализировал и настроил проект

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Text = styled.h1`
+  font-family: "Proxima Nova", sans-serif;
+`;
+
+const App: React.FC = () => {
+  return <Text>fawfaw</Text>;
+};
+
+export default App;
